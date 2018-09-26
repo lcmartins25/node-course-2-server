9370c94fe4eea1948453fff7fa45b42009838b42
Lua Carlos Martins <luacmartins@Luas-MacBook-Pro.local>
2018-09-25 23:00:52 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+//serve public folder
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+//middleware
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log')
+        }
+    })
+    next();
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// })
+
+
+
+hbs.registerHelper('currentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Welcome to my page'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Something went wrong'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000')
+});
